4bd04eecc6417ece21add8b5f3264b108c593924
matheauk <tk8141gu@go.minnstate.edu>
2024-09-05 09:38:19 -0500

    Added Form Functions

--- Exb1/FormInvoiceTotal.Designer.cs
+++ Exb1/FormInvoiceTotal.Designer.cs
@@ -1,14 +1,14 @@
 ﻿namespace Exb1
 {
-    partial class Form1
+    partial class FormInvoiceTotal
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -23,8 +23,8 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
@@ -43,7 +43,7 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(47, 31);
+            label1.Location = new Point(22, 23);
             label1.Name = "label1";
             label1.Size = new Size(54, 15);
             label1.TabIndex = 0;
@@ -53,7 +53,7 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(47, 60);
+            label2.Location = new Point(22, 52);
             label2.Name = "label2";
             label2.Size = new Size(100, 15);
             label2.TabIndex = 1;
@@ -63,7 +63,7 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(47, 89);
+            label3.Location = new Point(22, 81);
             label3.Name = "label3";
             label3.Size = new Size(104, 15);
             label3.TabIndex = 2;
@@ -73,7 +73,7 @@
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(47, 118);
+            label4.Location = new Point(22, 110);
             label4.Name = "label4";
             label4.Size = new Size(35, 15);
             label4.TabIndex = 3;
@@ -82,7 +82,7 @@
             // 
             // btnCalculate
             // 
-            btnCalculate.Location = new Point(79, 171);
+            btnCalculate.Location = new Point(61, 151);
             btnCalculate.Name = "btnCalculate";
             btnCalculate.Size = new Size(75, 23);
             btnCalculate.TabIndex = 2;
@@ -91,7 +91,7 @@
             // 
             // btnExit
             // 
-            btnExit.Location = new Point(180, 171);
+            btnExit.Location = new Point(169, 151);
             btnExit.Name = "btnExit";
             btnExit.Size = new Size(75, 23);
             btnExit.TabIndex = 3;
@@ -100,14 +100,14 @@
             // 
             // txtSubtotal
             // 
-            txtSubtotal.Location = new Point(159, 31);
+            txtSubtotal.Location = new Point(144, 23);
             txtSubtotal.Name = "txtSubtotal";
             txtSubtotal.Size = new Size(100, 23);
             txtSubtotal.TabIndex = 1;
             // 
             // txtDiscountPercent
             // 
-            txtDiscountPercent.Location = new Point(159, 60);
+            txtDiscountPercent.Location = new Point(144, 52);
             txtDiscountPercent.Name = "txtDiscountPercent";
             txtDiscountPercent.ReadOnly = true;
             txtDiscountPercent.Size = new Size(100, 23);
@@ -116,7 +116,7 @@
             // 
             // txtDiscountAmount
             // 
-            txtDiscountAmount.Location = new Point(159, 89);
+            txtDiscountAmount.Location = new Point(144, 81);
             txtDiscountAmount.Name = "txtDiscountAmount";
             txtDiscountAmount.ReadOnly = true;
             txtDiscountAmount.Size = new Size(100, 23);
@@ -125,20 +125,20 @@
             // 
             // txtTotal
             // 
-            txtTotal.Location = new Point(159, 118);
+            txtTotal.Location = new Point(144, 110);
             txtTotal.Name = "txtTotal";
             txtTotal.ReadOnly = true;
             txtTotal.Size = new Size(100, 23);
             txtTotal.TabIndex = 9;
             txtTotal.TabStop = false;
             // 
-            // Form1
+            // FormInvoiceTotal
             // 
             AcceptButton = btnCalculate;
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             CancelButton = btnExit;
-            ClientSize = new Size(313, 226);
+            ClientSize = new Size(300, 210);
             Controls.Add(txtTotal);
             Controls.Add(txtDiscountAmount);
             Controls.Add(txtDiscountPercent);
@@ -149,7 +149,8 @@
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
-            Name = "Form1";
+            Name = "FormInvoiceTotal";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Invoice Total";
             ResumeLayout(false);
             PerformLayout();
